4a7ce2f5b1c8717ce998e675d845b07b66030737
Philip Oesterlin <philip.oesterlin@gmail.com>
2025-03-24 12:25:44 +0100

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,13 +25,8 @@
 </script>
 
 <svelte:head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width" />
-	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-
 	<!-- Primary Meta Tags -->
 	<title>WasReal</title>
-
 	<meta name="title" content={title} />
 	<meta name="description" content={description} />
 
